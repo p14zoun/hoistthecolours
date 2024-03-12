f91d21b3f9c4a9fe605d8615243e5d2c293f3666
panagiotiszouggourides <100277385+panagiotiszouggourides@users.noreply.github.com>
2024-03-12 22:19:49 +0100

    m: nothing changed

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -12,7 +12,7 @@ function Card({ title, imageUrl, description, buttonText, link }: CardProps) {
   return (
     <div className="card w-15 shadow-xl">
       <a href={link}>
-        <div className="card-body bg-gray-800">
+        <div className="card-body bg-gray-800 ">
           <h2 className="card-title">{title}</h2>
           <p>{description}</p>
           <div className="card-actions">
