b431e8a08202873b3e7cd4e210bb49089d3383f9
panagiotiszouggourides <100277385+panagiotiszouggourides@users.noreply.github.com>
2024-03-12 22:43:25 +0100

    m: color card

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 
 function Card({ title, imageUrl, description, buttonText, link }: CardProps) {
   return (
-    <div className="card w-15 bg-gray-900 shadow-xl">
+    <div className="card w-15 bg-card shadow-xl">
       <a href={link}>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
